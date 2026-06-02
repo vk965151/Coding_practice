a8ac04d3b7a405fa3e040c1744d178c42f821a5c
Vishal Kashyap <vk965151@gmail.com>
2026-06-02 11:24:27 +0530

    practice problem

---
+++ array/prime.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int num;
+    cout<<"Enter number :";
+    cin>>num;
+    int count = 0;
+    for(int i=1; i<=num; i++){
+        if(num % i == 0){
+             count++;
+        }
+
+    }
+    if(count == 2){
+        cout<<"number is prime ";
+    }
+    else{
+        cout<<"number is not prime :";
+    }
+}
